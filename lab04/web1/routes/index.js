49eaeac50ee892e890823e83c3af50adaa53c50d
Long Nguyen Dinh Tran <longndt226@gmail.com>
2023-05-25 10:25:00 +0700

    1102

--- lab04/web1/routes/index.js
+++ lab04/web1/routes/index.js
@@ -11,19 +11,46 @@ router.get('/gch1102', (req, res) => {
   var class_name = 'gch1102';
   var module_name = 'cloud computing';
   var teacher_name = 'longndt';
-  res.render('gch1102', { lop_hoc : class_name, mon_hoc : module_name, giang_vien : teacher_name});
+  res.render('gch1102', { lop_hoc: class_name, mon_hoc: module_name, giang_vien: teacher_name });
 })
 
 router.get('/vietnam', (req, res) => {
   var vietnam = {
-    "name" : "Việt Nam",
-    "capital" : "Hà Nội",
-    "population" : "90 millions people",
-    "location" : "South East Asia",
-    "introduction" : "A peaceful country",
+    "name": "Việt Nam",
+    "capital": "Hà Nội",
+    "population": "90 millions people",
+    "location": "South East Asia",
+    "introduction": "A peaceful country",
     "image": "https://img.freepik.com/premium-vector/vietnam-map-landmarks-with-people-traditional-clothing_281368-239.jpg?w=2000"
   };
-  res.render("vietnam", { vietnam : vietnam});
+  res.render("vietnam", { vietnam: vietnam });
+})
+
+router.get('/employee', (req, res) => {
+  var employees =
+    [
+      {
+        "name": "tuan",
+        "age": 35,
+        "gender": "Male"
+      },
+      {
+        "name": "minh",
+        "age": 32,
+        "gender": "Male"
+      },
+      {
+        "name": "phuong",
+        "age": 30,
+        "gender": "Female"
+      },
+      {
+        "name": "hang",
+        "age": 25,
+        "gender": "Female"
+      }
+    ]
+   res.render("employee", { employees : employees}); 
 })
 
 module.exports = router;
