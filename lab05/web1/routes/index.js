6377e3764f509d23ca9cef74f2df37d48def7626
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-05-30 09:16:53 +0700

    1102

--- lab05/web1/routes/index.js
+++ lab05/web1/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/input', (req, res) => {
+  res.render('form')
+})
+
+router.post('/output', (req, res) => {
+  var data = req.body;
+  res.send(data);
+})
+
 module.exports = router;
