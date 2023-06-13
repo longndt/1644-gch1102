e5e9692ad18dd1bca6f950a88b6380a432414ea1
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-06-13 09:26:50 +0700

    1102

---
+++ lab08/web/routes/movie.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const MovieModel = require('../models/MovieModel');
+var router = express.Router();
+
+router.get('/', async (req, res) => {
+   var movies = await MovieModel.find({});
+   res.render('movie/index', { movies : movies });
+});
+
+module.exports = router;
