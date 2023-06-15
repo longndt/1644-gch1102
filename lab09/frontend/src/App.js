3c58f60d704c301c1d2df059b373a188236d9379
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-06-15 09:29:29 +0700

    1102

---
+++ lab09/frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world. we are learning ReactJS to consume API from ExpressJS</h1>
+      <img src="https://fullsuitcase.com/wp-content/uploads/2019/06/Things-to-do-in-Greenwich.jpg" alt="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab09/frontend/src/Greenwich.jsx
@@ -0,0 +1,18 @@
+function Greenwich() {
+  return (
+    <div>
+      <h1>Welcome to Greenwich Vietnam !</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/srlBwxXLyhk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default Greenwich;
